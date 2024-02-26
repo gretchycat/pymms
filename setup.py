5a95c9894d55b96bb2c586ca70b21a7e638c7120
Gretchen Maculo <gretchynm@gmail.com>
2024-02-26 03:03:52 -0600

    added first try trying to make it installable

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+import shutil
+
+with open('README.md') as f:
+    long_description = f.read()
+shutil.copyfile('pymms/termplayer.py', 'pymms/pymms')
+
+setup(
+    name='pymms',
+    version='0.0.1',
+    license='GPL3',
+    url='https://github.com/gretchycat/pymms',
+    author='Gretchen Maculo',
+    author_email='gretchen.maculo@gmail.com',
+    description='python xmms inspired media player/recorder',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pydub',
+        'pyte',
+    ],
+    tests_require=[
+    ],
+    scripts=['pymms/pymms']
+)
