60f00de6aab54a9f5ea719ba152677ee2090f9e6
Gretchen Maculo <gretchynm@gmail.com>
2024-02-26 03:32:31 -0600

    trying to get pip install working

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description='python xmms inspired media player/recorder',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages(),
+    packages=['pymms'],
     install_requires=[
         'pydub',
         'pyte',
